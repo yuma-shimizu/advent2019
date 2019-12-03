d16f9c08b47c845912b78f735effd8973a694491
yuma-shimizu <yuma.shimizu@axisware.co.jp>
2019-12-03 11:14:12 +0900

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
         <div className="logo">
             <span className="icon fa-diamond"></span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>AdventCalnder2019</h1>
+                <p>こんにちは、19日担当の平川です。<br />今回は私がお勧めするフロントエンド技術3選を紹介します。</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('intro') }}>GATSBY JS</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('work') }}>フロントエンド2</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('about') }}>フロントエンド3</a></li>
             </ul>
         </nav>
     </header>
