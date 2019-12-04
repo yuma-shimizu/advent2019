bef6bda0b35561bf4a2f2a4e346f40fa0adc57b6
yuma-shimizu <yuma.shimizu@axisware.co.jp>
2019-12-04 15:25:50 +0900

    vue design 完成

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'AxisAdventCalendar2019' },
+              { name: 'keywords', content: 'AxisAdventCalendar2019' },
             ]}
           >
             <html lang="en" />
